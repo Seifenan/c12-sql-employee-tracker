d8e28105722915684181bdfbc6bbc85e082f58de
S317 <seifenan@gmail.com>
2022-01-06 00:24:57 -0500

    modified schema & seeds sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,31 @@
-INSERT INTO parties (name, description)
-VALUES
-  ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaScript. They can build everything you could ever want in JS, including a new kitchen sink.'), 
-  ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in a matter of seconds.'), 
-  ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
+INSERT INTO department (name)
+VALUES 
+  ('Marketing'),
+  ('Sales'),
+  ('Legal'),
+  ('HR');
 
-INSERT INTO candidates (first_name, last_name, party_id, industry_connected)
+INSERT INTO role (title, salary, department_id)
 VALUES
-  ('Ronald', 'Firbank', 1, 1),
-  ('Virginia', 'Woolf', 1, 1),
-  ('Piers', 'Gaveston', 1, 0),
-  ('Charles', 'LeRoi', 2, 1),
-  ('Katherine', 'Mansfield', 2, 1),
-  ('Dora', 'Carrington', 3, 0),
-  ('Edward', 'Bellamy', 3, 0),
-  ('Montague', 'Summers', 3, 1),
-  ('Octavia', 'Butler', 3, 1),
-  ('Unica', 'Zurn', NULL, 1);
-  
+  1('CEO', 170000, 1),
+  2('Marketing Manager', 130000, 1),
+  3('Marketing Assistant', 80000, 1),
+  4('Sales Manager', 110000, 2),
+  5('Sales Assistant', 75000, 2),
+  6('Legal Manager', 120000, 3),
+  7('Legal Assistant', 75000, 3),
+  8('HR Manager', 100000, 4),
+  9('HR Assistant', 70000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Seif', 'Enan', 1, NULL),
+  ('Ronald', 'Firbank', 2, 1),
+  ('Virginia', 'Woolf', 3, 2),
+  ('Piers', 'Gaveston', 3, 2),
+  ('Charles', 'LeRoi', 4, 1),
+  ('Katherine', 'Mansfield', 5, 3),
+  ('Dora', 'Carrington', 6, 1),
+  ('Edward', 'Bellamy', 7, 4),
+  ('Montague', 'Summers', 8, 1),
+  ('Octavia', 'Butler', 9, 5);
